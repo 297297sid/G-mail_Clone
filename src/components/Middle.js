081dc257fc8c242f68bbb1dc15c86692c513f4bf
Sidak Bhatia <kaursidak297@gmail.com>
2024-02-27 03:56:36 +0530

    commit

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -13,6 +13,70 @@ function Middle() {
                     </ListItem>
                 </List>
             </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
+            <Paper elevation={0} style={{border:"1px solid lightgrey"}}>
+                <List>
+                    <ListItem>
+                        <img src={Starred} style={{width:"1.4vw",height:"1.4vw"}} />
+                        <span><span>hgbkjblb</span></span>
+                    </ListItem>
+                </List>
+            </Paper>
         </div>
     )
 }
