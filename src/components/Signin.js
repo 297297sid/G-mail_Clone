32770a718609984d6afd2a0602822b9edd5d0481
297297sid <kaursidak297@gmail.com>
2024-02-26 18:40:08 +0530

    new commit

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "@mui/material";
+import GoogleImage from "../images/google.png"
 
 function Signin(){
     return(
-        <div style={{position:"absolute" ,left:"35%",top:"40%"}}>
-            <div style={{border:"1px solid grey", padding:"20px",textAlign:"center"}}>
+        <div style={{position:"absolute" ,left:"35%",top:"25%"}}>
+            <div style={{border:"1px solid grey", padding:"20px",textAlign:"center",borderRadius:"5px"}}>
+                <img style={{width:"20%"}}src={GoogleImage}/>
             <h1 style={{fontWeight:"200"}}> Create your Google clone Account</h1>
             <h3 style={{fontWeight:"200"}}>Click the signin button</h3>
 
